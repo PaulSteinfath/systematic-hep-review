4f0149aae6c5f5fc55d37a255e5058c479c43842
Nikolai Kapralov <4dvlup@gmail.com>
2025-01-14 09:40:18 +0100

    Generate PRISMA diagram from screening data (#2)

--- functions/prisma.R
+++ functions/prisma.R
@@ -8,13 +8,79 @@ plot_prisma_diagram <- function(df_prisma) {
 }
 
 
-generate_prisma <- function(df_screening, prisma_path, save_path) {
-  # TODO: generate automatically based on the template and the screening data
-  df_prisma <- read.csv(prisma_path)
+reasons_to_exclude <- function(df_excluded) {
+  ignore <- c("Consider", "Duplicate", "Preprint")
+ 
+  reasons <- df_excluded %>%
+    filter(Freq > 0) %>%
+    filter(!(Comment %in% ignore)) %>%
+    select(c("Comment", "Freq")) %>%
+    arrange(-Freq, decreasing = T) %>%
+    mutate(Desc = paste0(Comment, ", ", Freq))
+  
+  paste(reasons$Desc, collapse = "; ") 
+}
+
+
+generate_prisma <- function(df_screening, template_path, derivatives_path, plot_path) {
+  # Process all reasons to exclude
+  comments <- with(df_screening, as.data.frame(table(source, Comment, Include)))
+  excluded <- comments[comments$Include == 0,]
+  excluded_pubmed <- excluded[excluded$source == "pubmed",]
+  excluded_manual <- excluded[excluded$source == "manual",]
+  reasons_pubmed <- reasons_to_exclude(excluded_pubmed)
+  reasons_manual <- reasons_to_exclude(excluded_manual)
+  
+  # NOTE: duplicates and preprints only appear in Pubmed results
+  num_duplicates <- excluded_pubmed$Freq[excluded_pubmed$Comment == "Duplicate"]
+  num_preprints <- excluded_pubmed$Freq[excluded_pubmed$Comment == "Preprint"]
+  
+  # Prepare all numbers
+  num_pubmed <- nrow(df_screening[df_screening$source == "pubmed",])
+  num_screened <- num_pubmed - num_preprints - num_duplicates
+  num_manual <- nrow(df_screening[df_screening$source == "manual",])
+  num_included <- nrow(df_screening[df_screening$Include == 1,])
+  
+  # Fill in the template PRISMA CSV file
+  df_prisma <- read.csv(template_path)
+  
+  # Pubmed
+  df_prisma$n[df_prisma$data == "database_specific_results"] <- paste("Pubmed,", num_pubmed)
+  df_prisma$n[df_prisma$data == "records_screened"] <- num_screened
+  df_prisma$n[df_prisma$data == "dbr_sought_reports"] <- num_screened
+  df_prisma$n[df_prisma$data == "dbr_assessed"] <- num_screened
+  df_prisma$n[df_prisma$data == "dbr_excluded"] <- reasons_pubmed
+  df_prisma$n[df_prisma$data == "new_studies"] <- num_included
+  df_prisma$n[df_prisma$data == "duplicates"] <- num_duplicates
+  df_prisma$n[df_prisma$data == "excluded_other"] <- num_preprints
+  
+  # Manual
+  df_prisma$n[df_prisma$data == "citations_results"] <- num_manual
+  df_prisma$n[df_prisma$data == "other_sought_reports"] <- num_manual
+  df_prisma$n[df_prisma$data == "other_assessed"] <- num_manual
+  df_prisma$n[df_prisma$data == "other_excluded"] <- reasons_manual
+  
+  # Remove unnecessary parts of the visualization by making the corresponding 
+  # rows empty
+  df_prisma$n[df_prisma$data == "database_results"] <- NA
+  df_prisma$n[df_prisma$data == "register_results"] <- NA
+  df_prisma$n[df_prisma$data == "website_results"] <- NA
+  df_prisma$n[df_prisma$data == "organisation_results"] <- NA
+  df_prisma$n[df_prisma$data == "new_reports"] <- NA
+  df_prisma$n[df_prisma$data == "excluded_automatic"] <- NA
+  
+  # Adjust the box text
+  df_prisma$boxtext[df_prisma$data == "excluded_other"] <- "Preprints"
+  df_prisma$boxtext[df_prisma$data == "identification"] <- "Search"
+  
+  # Save the resulting dataframe
+  write.csv(df_prisma, file.path(derivatives_path, 'PRISMA.csv'), row.names = F)
+  
+  # Plot the resulting diagram
   p_prisma <- plot_prisma_diagram(df_prisma)
   PRISMA_save(
     p_prisma, 
-    file.path(save_path, "PRISMA.png"),
+    file.path(plot_path, "PRISMA.svg"),
     overwrite = T
   )
 }

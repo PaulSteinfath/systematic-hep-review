669d5f009ad7201498902f1d14f10c256a5f73fd
Nikolai Kapralov <4dvlup@gmail.com>
2025-04-02 15:29:32 +0200

    Keep unknowns for the number of ECG electrodes

--- functions/utils.R
+++ functions/utils.R
@@ -1,5 +1,3 @@
-library(tools)
-
 get_check_sums <- function(results_path) {
   checksums <- tools::md5sum(dir(results_path, full.names = T))
   df_sums <- data.frame(filename = unlist(lapply(names(checksums), basename)), 

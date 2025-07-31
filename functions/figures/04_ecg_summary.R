dcaf91f4c74b3eb1bfbf875a7ec0696af603a8f4
Nikolai Kapralov <4dvlup@gmail.com>
2025-07-31 06:39:10 +0200

    Rename ECG figure

--- functions/figures/04_ecg_summary.R
+++ functions/figures/04_ecg_summary.R
@@ -32,7 +32,7 @@ figure_ecg_summary <- function(df, save_path, ext = 'png') {
   )
   
   ggsave(
-    filename = file.path(save_path, paste0("ecg_summary_plot.", ext)),
+    filename = file.path(save_path, paste0("fig4_ecg_summary.", ext)),
     plot = fig,
     width = 10,
     height = 4,

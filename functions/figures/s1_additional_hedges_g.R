2849e20b3c49ffffd2c71912c8dbcb95d6f6dce4
Nikolai Kapralov <4dvlup@gmail.com>
2025-07-31 09:12:24 +0200

    Extract supplementary Hedges' g plot

---
+++ functions/figures/s1_additional_hedges_g.R
@@ -0,0 +1,14 @@
+figure_additional_hedges_g <- function(df, save_path, ext = 'svg') {
+  fig <- plot_hedges_g(df = df, with_clustering = T, with_regression = T)
+  
+  ggsave(
+    filename = file.path(save_path, paste0("additional_hedges_g_plot.", ext)),
+    plot = fig,
+    width = 7,
+    height = 5,
+    units = "in",
+    dpi = 300,
+    device = ext,
+    bg = "white"
+  )
+}

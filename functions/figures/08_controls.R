832652590d986e0c4ac4816b04605bda73e8e975
PaulSteinfath <steinfath@cbs.mpg.de>
2025-07-31 13:23:42 +0200

    controls & supplement

--- functions/figures/08_controls.R
+++ functions/figures/08_controls.R
@@ -13,9 +13,9 @@ figure_controls <- function(df, save_path, ext = 'png') {
   ggsave(
     filename = file.path(save_path, paste0("fig8_controls.", ext)),
     plot = p,
-    width = 10,
-    height = 3.5,
-    units = "in",
+    width = 190,
+    height = 88.9,
+    units = "mm",
     dpi = 300,
     device = ext,
     bg = "white"

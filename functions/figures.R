bbba7018bea4050b132be38cef749ea330a4831d
Nikolai Kapralov <4dvlup@gmail.com>
2025-07-31 05:56:05 +0200

    Extract Fig.4

--- functions/figures.R
+++ functions/figures.R
@@ -28,6 +28,7 @@ source(file.path(func_path, "plots", "plot_simulated_effects.R"))
 
 source(file.path(func_path, 'figures', '01_overview_studies.R'))
 source(file.path(func_path, 'figures', '02_overview_pipelines.R'))
+source(file.path(func_path, 'figures', '04_ecg_summary.R'))
 source(file.path(func_path, 'figures', '06_hep_estimation.R'))
 source(file.path(func_path, 'figures', '07_stats.R'))
 source(file.path(func_path, 'figures', '08_controls.R'))
@@ -204,54 +205,6 @@ cfa_removal <- function(df) {
 
   plot_grid(top_row, middle_row, bottom_row, ncol = 1)
 }
-
-
-figure_ecg_summary <- function(df, save_path, ext = 'png') {
-  # Map "unknown" to 9 so that it isn't lost during conversion to numeric and
-  # is positioned nicely
-  df <- df %>%
-    mutate(ecg_num_electrodes = replace(ecg_num_electrodes,
-                                        ecg_num_electrodes == "unknown", 9))
-  p_ecg_num_electrodes <- hist_panel(df, "ecg_num_electrodes", 
-                                     force.numeric = T, binwidth = 1,
-                                     x.label = "Number of ECG electrodes",
-                                     title = "Number of ECG electrodes") +
-    scale_x_continuous(breaks = seq(0, 9),
-                       labels = c(seq(0, 8), "N/M"))
-  p_ecg_leads <- hist_panel(df, "ecg_lead", fill_as_aesthetic = T,
-                            discrete = T, title = "ECG lead") +
-    scale_fill_manual(values = leads_palette,
-                      na.value = plot_fill_default_single,
-                      guide = "none") 
-  p_ecg_locations <- plot_ecg_locations(df, leads_palette)
-  fig_AB = plot_grid(
-    p_ecg_num_electrodes,
-    p_ecg_leads,
-    ncol = 1,
-    labels = c("A", "B")
-  )
-
-  fig <- plot_grid(
-    fig_AB,
-    p_ecg_locations,
-    nrow = 1,
-    rel_widths = c(1, 1.5),
-    labels = c("", "C")
-  )
-  
-  ggsave(
-    filename = file.path(save_path, paste0("ecg_summary_plot.", ext)),
-    plot = fig,
-    width = 10,
-    height = 4,
-    units = "in",
-    dpi = 300,
-    device = ext,
-    bg = "white"
-  )
-}
-
-
 
 
 # Here we generate all figures

87045d1b65d1671e6f0dd9a90783987930465642
Nikolai Kapralov <4dvlup@gmail.com>
2025-07-31 10:18:27 +0200

    Rename the output figure

--- functions/figures/s3_control_variables.R
+++ functions/figures/s3_control_variables.R
@@ -2,7 +2,7 @@ figure_control_variables <- function(df, save_path, ext = 'svg') {
   fig <- create_control_variables_plot(df)
   
   ggsave(
-    filename = file.path(save_path, paste0("control_variables_plot.", ext)),
+    filename = file.path(save_path, paste0("figS3_control_variables.", ext)),
     plot = fig,
     width = 7,
     height = 12,

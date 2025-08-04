74828b0ee654e67ea534c633cf38dd382b8da9bf
Nikolai Kapralov <4dvlup@gmail.com>
2025-08-04 14:11:58 +0200

    Extract figure S2

---
+++ functions/config/config.R
@@ -0,0 +1,12 @@
+# Analysis parameters
+params <- list(
+  entropy_unique_threshold = 10,
+  entropy_num_bins = 10,
+  hedges_sig_level = 0.05,
+  hedges_power_level = 0.8,
+  r_peak_offset = 0,
+  t_peak_offset = 300
+)
+
+t_peak_offset <- 300
+r_peak_offset <- 0

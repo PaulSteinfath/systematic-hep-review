f8b6485b63e7e3e18e8c7d9ae45a6812e5108e6f
Nikolai Kapralov <4dvlup@gmail.com>
2025-08-05 14:37:38 +0200

    Figure 7 & S1 update (#39)

--- functions/plots/plotting_helpers.R
+++ functions/plots/plotting_helpers.R
@@ -14,17 +14,17 @@ plot_fill_default_single <- "#696969"
 
 plot_fill_default <- c("#696969","#A9A9A9","#8a8888")
 
-effect_sizes_Coll2020 <- data.frame(
+effect_sizes_Coll2021 <- data.frame(
   kind = c("Attention to the heart",
            "Interoceptive performance",
            "Arousal",
-           "Patients vs. healthy controls"),
+           "Clinical vs. control groups"),
   value = c(0.37, 0.35, 0.72, 0.49)
 )
-palette_Coll2020 <- c("Attention to the heart" = "#1b9e77",
+palette_Coll2021 <- c("Attention to the heart" = "#1b9e77",
                       "Interoceptive performance" = "#d95f02",
                       "Arousal" = "#7570b3",
-                      "Patients vs. healthy controls" = "#e7298a")
+                      "Clinical vs. control groups" = "#e7298a")
 
 theme_set(plot_theme_default)
 
